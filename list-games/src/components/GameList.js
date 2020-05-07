a6d2a3ed6f8b82d0880606a2b977258dfef2d378
Claire Kodia <claire.kodia@gmail.com>
2020-05-07 10:47:12 +0200

    Managed to get data from the api using axios and to display as many card as necessary

--- list-games/src/components/GameList.js
+++ list-games/src/components/GameList.js
@@ -4,10 +4,23 @@ import axios from "axios";
 import Game from "./Game";
 
 class GameList extends Component {
+    state = {
+        GameFullList: []
+    }
+
+    componentDidMount() {
+        this.getGameList()
+    }
+
+    getGameList = () => {
+        axios.get("https://wild-games.herokuapp.com/api/v1")
+        .then(res => this.setState(() => ({GameFullList: res.data}), () => console.log(this.state.GameFullList)))
+    }
+
     render() {
         return (
             <div>
-                <Game name="game name" image="https://picsum.photos/200/300" rating="rating" />
+                {this.state.GameFullList.map(game => <Game name={game.name} image={game.background_image} rating={game.rating} />)}
             </div>
         )
     }
